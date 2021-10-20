1c5aea69de50f3dfb0125fb734704c159c35316c
videobuff <fhaschott@gmail.com>
2021-10-20 16:16:46 +0200

    Update Servo-dem0-2.ino

--- Servo-dem0-2.ino
+++ Servo-dem0-2.ino
@@ -3,17 +3,17 @@
  * sketch name: Servo-demo-2
  * Last updated 21-10-201
 
-  Description:  Demonstratie model om met een togle schakelaar een servo van links naar 
+  Description:  Demonstratie model om met een togle schakelaar een servo van links naar
                 rechts te laten bewegen, bv voor een overweg of een sein.
                 Maakt gebruik van de arduino uno en de pca9865
 
-  Software:     Developed using Arduino 1.8.18 software for the Ardunino INO  
-                Should be compatible with Arduino 1.0 + and newer ESP32/Arduino releases if any    
+  Software:     Developed using Arduino 1.8.18 software for the Ardunino INO
+                Should be compatible with Arduino 1.0 + and newer ESP32/Arduino releases if any
 
-  ToDo          Timing fine tunen, schakelaar debouncen etc.    
-  
+  ToDo          Timing fine tunen, schakelaar debouncen etc.
+
   Date:         21-102021
- 
+
   Author:       Erik Schott - erik@pa0esh.com
 */
 
@@ -29,22 +29,25 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define SERVOMIN  130 // this is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX  650 // this is the 'maximum' pulse length count (out of 4096)
 
-// our servo # counter
+// Onze servo # counter - we beginnen met kanaal 0 op de pca9865
 uint8_t servonum = 0; //zorg later voor een loop om iedere servo aan te spreken. Test daarvoor de wijziging in een schakelaar waarde.
-// schakelaar definitie
+// Status van de servo (linksom of rechtsom)
+int status_servo0 = 0;
+// sla status op. Draai alleen servo als status wijzigt. 0 = links - 1 = rechts
 int switch_pin0 = 4;
 // sluit hier de eerste schakelaar op aan, die servo 0 bedient. digitale pin 4
 // schakelaar is als volgt: pin 4 met een weerstand van 1k naar plus 5V. Middenpoot van de schakelaar aan aarde.
 // rechter / of linker pootje naar pin 4 op de Arduino.
 
-int status_servo0 = 0;
-// sla status op. Draai alleen servo als status wijzigt. 0 = links - 1 = rechts
+
 
 byte leds = 0;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("16 channel Servo test!");
+  Serial.println("Modelbaan Servo test!");
+  Serial.println("voor Hans Schaap!");
+  Serial.println("");
   pwm.begin();
   pwm.setPWMFreq(60);  // Analog servos werken met ~60 Hz updates
   pinMode(switch_pin0, INPUT);
@@ -57,13 +60,13 @@ void setup() {
  * written by Ahmad Nejrabi for Robojax, Robojax.com
  */
 int angleToPulse(int ang){
-   int pulse = map(ang,0, 180, SERVOMIN,SERVOMAX);// map angle of 0 to 180 to Servo min and Servo max 
+   int pulse = map(ang,0, 180, SERVOMIN,SERVOMAX);// map angle of 0 to 180 to Servo min and Servo max
    //Serial.print("Angle: ");Serial.print(ang);
    //Serial.print(" pulse: ");Serial.println(pulse);
    return pulse;
 }
 void turn_left(){
-  
+
    for( int angle =0; angle<181; angle +=5){
     delay(50);
     pwm.setPWM(0, 0, angleToPulse(angle) );
@@ -72,8 +75,8 @@ void turn_left(){
 }
 
 void turn_right(){
-  
-  for( int angle =181; angle>10; angle -=5){
+
+  for( int angle =181; angle>5; angle -=5){
     delay(50);
     pwm.setPWM(0, 0, angleToPulse(angle) );
   }
@@ -87,26 +90,22 @@ void check_switch(){
       //Serial.println(" No change detected" );
       return;
     } else {
-      Serial.println(" Change detected" );
+      //Serial.println(" Change detected" );
       status_servo0=digitalRead(switch_pin0);
-      Serial.print("De status van pin 4 = :");
+      //Serial.print("De status van pin 4 = :");
       Serial.println(digitalRead(switch_pin0));
 
       if (status_servo0 == 1){
-       turn_left(); 
+       turn_left();
       } else {
         turn_right();
       }
-      
+
     }
   }
-  
+
 // De code binnen de void loop() functie moet je aanpasen voor wat je wilt. Ik test de positie van de schakelaars iedere keer en doe dan wat
 void loop() {
 check_switch();
-delay(10);
 
 }
-
-
-  
